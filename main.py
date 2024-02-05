47bdc63fd7dc11e2946d934ac900fe5c02c02028
SYMMETRY4U <121978473+SYMMETRY4U@users.noreply.github.com>
2024-02-05 18:28:29 +0000

    updates2-2-24

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template,request,url_for
 #create a secret key for security
 import os
 
+import json
+
 import utils as util
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key')
 
 @app.route('/')
 def index():
@@ -12,34 +16,78 @@ def index():
     return render_template("index.html", title=title)
 
 
-
-
 @app.route('/about')
 def about():
     title = "About"
     return render_template("about.html", title=title)
 
+
+@app.route('/users')
+def users():
+  # Read project data from JSON file
+  with open('test.json') as json_file:
+      user_data = json.load(json_file)
+      # print(user_data)
+      context = {
+        "title": "Users",
+        "users": user_data
+    }
+  return render_template("users.html",**context)
+
+
+@app.route('/user/<int:user_number>')
+def show_user(user_number):
+  this_user = load_user_data(user_number)
+  if this_user:
+      title = "User"
+      context = {
+        "title": title,
+        "user":  this_user
+      }
+      return render_template("user.html", **context)
+  else:
+      return 'User not found', 404
+
+def load_user_data(user_number):
+  # Read project data from JSON file
+  with open('test.json') as json_file:
+      user_data = json.load(json_file)
+      user = next((u for u in user_data if u['id'] == user_number), None)
+      return user
+
+
+
 # Route for the form page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    title = "Register"
-    feedback = None
-    if request.method == 'POST':
-        feedback = register_data(request.form)
+      title = "Register"
+      feedback = None
+      if request.method == 'POST':
+          form_data = request.form.to_dict()
+          util.title_case_fields(form_data)  # Call the function to modify the data
+          feedback = register_data(form_data)
 
-    context = {
-        "title": title,
-        "feedback": feedback
-    }
-    return render_template('register.html', **context)
+      context = {
+          "title": title,
+          "feedback": feedback
+      }
+      return render_template('register.html', **context)
 
 
 def register_data(form_data):
-    feedback = []
-    for key, value in form_data.items():
-        feedback.append(f"{key}: {value}")
-    return feedback
-
+  feedback = []
+  for key, value in form_data.items():
+    # checkboxes have [] for special handling
+    if key.endswith('[]'):
+      # Use getlist to get all values for the checkbox
+      checkbox = request.form.getlist(key)
+      key = key.replace('_', ' ').replace('[]', '')
+      feedback.append(f"{key}: {', '.join(map(str, checkbox))}")
+    else:
+      # Handle other form elements
+      key = key.replace('_', ' ')
+      feedback.append(f"{key}: {value}")
+  return feedback
 
 @app.route('/motorcycles')
 def motorcycles():
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 def movie_stars(movie_dict):
   for movie in movie_dict:
       movie['stars'] = add_stars(movie['Rating'])
@@ -16,3 +15,10 @@ def add_stars(rating):
       my_return += f"<span class=\"fa fa-star {checked}\"></span>"
   return my_return
 
+def title_case_fields(data):
+  for key in data:
+      match key:
+          case 'First_Name' | 'Last_Name' | 'Address_Line1' | 'Address_Line2' | 'City':
+              data[key] = data[key].title()
+          case _:
+              continue
